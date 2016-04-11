ebd15d423b504597fb90c76f4b4295ba5a34a521
SB <amruthasuri@gmail.com>
2016-04-11 13:29:02 +0530

    storyteller: https://basecamp.com/2947346/projects/12010730 - Nouns.do and ShowJ2eeStoryAboutNouns.do producing json response
    storyteller: https://basecamp.com/2947346/projects/12010730 - Nouns.do
    and ShowJ2eeStoryAboutNouns.do producing json response

---
+++ storyteller/src/main/java/net/storyteller/web/util/JSONResponseUtil.java
@@ -0,0 +1,27 @@
+package net.storyteller.web.util;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import net.enclosing.util.JSONUtil;
+
+public class JSONResponseUtil {
+
+	private static final String APPLICATION_JSON = "application/json; charset=utf-8";
+	
+	public static void writeResponseAsJSON(HttpServletResponse response,Object o) throws IOException {
+		response.setContentType(APPLICATION_JSON);
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.getWriter().print(JSONUtil.toJSON(o));
+		response.flushBuffer();
+	}
+	
+/*	public static void sendErrorJSON(HttpServletResponse res,Errors errors) throws IOException {
+		res.setContentType(APPLICATION_JSON);		
+		res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		res.getWriter().print(JSONUtil.serialize(errors));
+		res.flushBuffer();
+	}	
+*/
+}
